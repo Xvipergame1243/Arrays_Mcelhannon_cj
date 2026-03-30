282b6479f3fc1ea345310b870c0e959a516785db
Xvipergame1243 <cj.mcelhannon@wpstigers.k12.ok.us>
2026-03-30 08:37:11 -0500

    Initial commit

---
+++ Arrays_Mcelhannon_cj/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Arrays_Mcelhannon_cj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a wholle number for any array size");
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[arraySize];
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("enter a whole number for the array");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+                sum += numbers[i];
+            }
+            Console.WriteLine($"the sum of the array is {sum}");
+
+            Array.Sort(numbers);
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Arrays_Mcelhannon_cj
+
